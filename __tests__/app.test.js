96f4c26712cfcb6ef78b8ca700c3fa1f496a3faf
Челнокова Александра Витальевна <chelalex1511@gmail.com>
2024-11-17 21:48:37 +0300

    changed app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,13 +1,23 @@
 // __tests__/app.test.js
-const http = require('http');
-const request = require('supertest');
-const app = require('../app');  // Импортируем сервер
 
-const server = http.createServer(app);
+const request = require('supertest'); // предполагаем, что используете supertest
+const app = require('../app'); // Путь к вашему файлу с сервером
 
-jest.setTimeout(10000);
+jest.setTimeout(15000); // Увеличиваем таймаут для всех тестов до 15 секунд
 
 describe('GET /', () => {
+  let server;
+
+  beforeAll((done) => {
+    // Запуск сервера до выполнения тестов
+    server = app.listen(3000, () => done());
+  });
+
+  afterAll((done) => {
+    // Закрытие сервера после завершения всех тестов
+    server.close(done);
+  });
+
   it('responds with Hello, World!', async () => {
     const response = await request(server).get('/');
     expect(response.statusCode).toBe(200);
