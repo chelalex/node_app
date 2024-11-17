d2a42eba06f1311391427b4f2d1ee23c802712df
Челнокова Александра Витальевна <chelalex1511@gmail.com>
2024-11-17 21:52:08 +0300

    changed app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,27 +1,10 @@
 // __tests__/app.test.js
+const request = require('supertest');
+const app = require('../app');  // Импорт вашего приложения
 
-const request = require('supertest'); // предполагаем, что используете supertest
-const app = require('../app'); // Путь к вашему файлу с сервером
-
-jest.setTimeout(15000); // Увеличиваем таймаут для всех тестов до 15 секунд
-
-describe('GET /', () => {
-  let server;
-
-  beforeAll((done) => {
-    // Запуск сервера до выполнения тестов
-    server = app.listen(3000, () => done());
-  });
-
-  afterAll((done) => {
-    // Закрытие сервера после завершения всех тестов
-    server.close(done);
-  });
-
-  it('responds with Hello, World!', async () => {
-    const response = await request(server).get('/');
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('Hello, World!\n');
-  });
+it('responds with Hello, World!', async () => {
+  const response = await request(app).get('/'); // Просто вызываем app, чтобы получить запрос
+  expect(response.statusCode).toBe(200);
+  expect(response.text).toBe('Hello, World!\n');
 });
 
